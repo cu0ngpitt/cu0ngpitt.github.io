2e804ff839e8c2516003b57851e9f19185ef1a88
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-06-05 15:05:39 -0500

    Initial Commit from generating a new project, included my necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { EducationComponent } from './components/education/education.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SkillsComponent,    
+    EducationComponent,
+    AboutMeComponent,
+    ContactComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
