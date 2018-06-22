d38e5fef51fecda0803f1d5dbd7a66427b8f7bb8
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-06-22 14:24:31 -0500

    Subscribed to my projects data

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Project } from '../../models/project';
+import { ProjectService } from '../../services/project.service';
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+  tasks: Project[];
 
-  constructor() { }
+  constructor(private projectServ: ProjectService) { }
 
   ngOnInit() {
+    this.getProjects();
+  }
+
+  getProjects(): void {
+    this.projectServ.getProjects()
+      .subscribe((project: any) => this.tasks = project.projectData);
   }
 
 }
