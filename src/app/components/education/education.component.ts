8329fd04ce839a6699cff61386efd2d9977aa009
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-06-22 14:18:49 -0500

    Subscribed to my education data

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Education } from '../../models/education';
+import { EducationService } from '../../services/education.service';
+
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.css']
 })
 export class EducationComponent implements OnInit {
+  codeschool: Education[];
+  egghead: Education[];
 
-  constructor() { }
+  constructor(private edService: EducationService) { }
 
   ngOnInit() {
+    this.getEds();
   }
 
+  getEds(): void {
+    this.edService.getEds()
+      .subscribe((eds: any) => {
+        this.codeschool = eds.educationData[0].codeschool;
+        this.egghead = eds.educationData[0].egghead;
+      });
+  }
 }
