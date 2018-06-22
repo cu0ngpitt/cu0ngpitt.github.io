d64a5583f1a7d1c2843ce7f0bfc2474217206952
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-06-22 14:15:32 -0500

    Added routing paths and imported router and http modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
@@ -10,19 +12,39 @@ import { AboutMeComponent } from './components/about-me/about-me.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 
+import { SkillsService } from './services/skills.service';
+import { EducationService } from './services/education.service';
+import { ProjectService } from './services/project.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'contact', component: ContactComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
-    SkillsComponent,    
+    SkillsComponent,
     EducationComponent,
     AboutMeComponent,
     ContactComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [SkillsService, EducationService, ProjectService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
