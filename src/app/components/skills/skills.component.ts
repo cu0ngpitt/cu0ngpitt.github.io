3740d1d8e22964954afdf616da81cf8712cb9336
Cuong Vo <36747471+cu0ngpitt@users.noreply.github.com>
2018-06-22 14:25:35 -0500

    Subscribed to my skills data

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Skills } from '../../models/skills';
+import { SkillsService } from '../../services/skills.service';
+
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent implements OnInit {
+  skills: Skills[];
 
-  constructor() { }
+  constructor(private skillsService: SkillsService) { }
 
   ngOnInit() {
+    this.getSkills();
+  }
+
+  getSkills(): void{
+    this.skillsService.getSkills()
+      .subscribe((skills: any) => this.skills = skills.skillsData);
   }
 
 }
